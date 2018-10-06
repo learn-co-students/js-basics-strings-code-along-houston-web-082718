8fcd70b61cc8dcbdfccd8525f1ae3491696c5811
gwatson86 <gwatson86@gmail.com>
2018-10-06 12:08:51 -0500

    actually coded along this time

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const currentUser = "Grace Hopper";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser[0]}!` 
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` 
